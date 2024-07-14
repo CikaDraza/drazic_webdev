f1d58191206f0acd072984ac5928c371a2172901
CikaDraza <drazic.milan@gmail.com>
2024-07-14 16:28:24 +0200

    add profile pic

--- src/App.jsx
+++ src/App.jsx
@@ -80,13 +80,13 @@ function App() {
                 </defs>
                 <g filter="url(#filter0_d)">
                   <image
-                    xlinkHref="https://images.unsplash.com/photo-1711000142551-ca65827c57c3?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" // Make sure to replace with your image path
-                    width="654"
-                    height="716"
+                    xlinkHref="/profilePhoto.jpg"
+                    width="580"
+                    height="660"
                     clipPath="url(#clip0)"
                     preserveAspectRatio="xMidYMid slice"
                     x="50" // Adjust x to move left or right
-                    y="35" // Adjust y to move up or down
+                    y="0" // Adjust y to move up or down
                   />
                 </g>
               </svg>
