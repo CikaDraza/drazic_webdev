a5f28644d1b4f425b083d39233d483106a0b2cd2
CikaDraza <drazic.milan@gmail.com>
2024-10-29 05:19:13 +0100

    pass header to api

--- src/utils/api/contact/index.js
+++ src/utils/api/contact/index.js
@@ -6,7 +6,11 @@ export const sendContactForm = async (contactData) => {
   console.log('on api', contactData);
   
   try {
-    const response = await axios.post(`${API_BASE_URL}/send-email`, contactData);
+    const response = await axios.post(`${API_BASE_URL}/send-email`, contactData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
     return response.data;
   } catch (error) {
     console.error('Contact form submission error:', error);
